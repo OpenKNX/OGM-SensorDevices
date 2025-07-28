7eb89a1eb31314b928440c43347dcc032edaa4c9
Waldemar Porscha <wp@porscha.eu>
2025-07-28 17:17:02 +0200

    Get rid of delays for PT1000

--- src/SensorMAX31865.cpp
+++ src/SensorMAX31865.cpp
@@ -1,7 +1,7 @@
 // Sensor MAX31865 - PT1000 interface
+#include "SensorMAX31865.h"
 #if defined(SENSORMODULE) && defined(OPENKNX_SPI_MISO)
     #include "SPI.h"
-    #include "SensorMAX31865.h"
 
 SensorMAX31865::SensorMAX31865(uint16_t iMeasureTypes, TwoWire* iWire)
     : Sensor(iMeasureTypes, iWire, 0){};
@@ -110,8 +110,14 @@ bool SensorMAX31865::begin()
         SPI.setCS(OPENKNX_SPI_CS);
         SPI.setSCK(OPENKNX_SPI_SCK);
         SPI.setTX(OPENKNX_SPI_MOSI); // or setMOSI()
-        mPT1000 = new Adafruit_MAX31865(OPENKNX_SPI_CS,&SPI);
-        lResult = mPT1000->begin(MAX31865_3WIRE);
+        spi_dev = new Adafruit_SPIDevice(OPENKNX_SPI_CS, 1000000, SPI_BITORDER_MSBFIRST, SPI_MODE1, &SPI);
+        spi_dev->begin();
+
+        setWires(MAX31865_3WIRE);
+        enableBias(false);
+        autoConvert(false);
+        setThresholds(0, 0xFFFF);
+        clearFault();
     }
     logResult(lResult);
     return lResult;
@@ -124,16 +130,127 @@ uint8_t SensorMAX31865::getI2cSpeed()
 
 bool SensorMAX31865::getSensorData()
 {
-    mTemp = mPT1000->calculateTemperature(doReadRTD(),RNOMINAL, RREF);
-    uint8_t fault = mPT1000->readFault();
+    mTemp = calculateTemperature(doReadRTD(),RNOMINAL, RREF);
+    uint8_t fault = readFault();
 
     return (fault == 0);
 }
 
+void SensorMAX31865::setWires(max31865_numwires_t wires) {
+  uint8_t t = readRegister8(MAX31865_CONFIG_REG);
+  if (wires == MAX31865_3WIRE) {
+    t |= MAX31865_CONFIG_3WIRE;
+  } else {
+    // 2 or 4 wire
+    t &= ~MAX31865_CONFIG_3WIRE;
+  }
+  writeRegister8(MAX31865_CONFIG_REG, t);
+}
+
+void SensorMAX31865::enableBias(bool b) {
+  uint8_t t = readRegister8(MAX31865_CONFIG_REG);
+  if (b) {
+    t |= MAX31865_CONFIG_BIAS; // enable bias
+  } else {
+    t &= ~MAX31865_CONFIG_BIAS; // disable bias
+  }
+  writeRegister8(MAX31865_CONFIG_REG, t);
+}
+
+void SensorMAX31865::autoConvert(bool b) {
+  uint8_t t = readRegister8(MAX31865_CONFIG_REG);
+  if (b) {
+    t |= MAX31865_CONFIG_MODEAUTO; // enable autoconvert
+  } else {
+    t &= ~MAX31865_CONFIG_MODEAUTO; // disable autoconvert
+  }
+  writeRegister8(MAX31865_CONFIG_REG, t);
+}
+
+void SensorMAX31865::setThresholds(uint16_t lower, uint16_t upper) {
+  writeRegister8(MAX31865_LFAULTLSB_REG, lower & 0xFF);
+  writeRegister8(MAX31865_LFAULTMSB_REG, lower >> 8);
+  writeRegister8(MAX31865_HFAULTLSB_REG, upper & 0xFF);
+  writeRegister8(MAX31865_HFAULTMSB_REG, upper >> 8);
+}
+
+void SensorMAX31865::clearFault(void) {
+  uint8_t t = readRegister8(MAX31865_CONFIG_REG);
+  t &= ~0x2C;
+  t |= MAX31865_CONFIG_FAULTSTAT;
+  writeRegister8(MAX31865_CONFIG_REG, t);
+}
+
+float SensorMAX31865::calculateTemperature(uint16_t RTDraw, float RTDnominal, float refResistor) {
+  float Z1, Z2, Z3, Z4, Rt, temp;
+
+  Rt = RTDraw;
+  Rt /= 32768;
+  Rt *= refResistor;
+
+  // Serial.print("\nResistance: "); Serial.println(Rt, 8);
+
+  Z1 = -RTD_A;
+  Z2 = RTD_A * RTD_A - (4 * RTD_B);
+  Z3 = (4 * RTD_B) / RTDnominal;
+  Z4 = 2 * RTD_B;
+
+  temp = Z2 + (Z3 * Rt);
+  temp = (sqrt(temp) + Z1) / Z4;
+
+  if (temp >= 0)
+    return temp;
+
+  // ugh.
+  Rt /= RTDnominal;
+  Rt *= 100; // normalize to 100 ohm
+
+  float rpoly = Rt;
+
+  temp = -242.02;
+  temp += 2.2228 * rpoly;
+  rpoly *= Rt; // square
+  temp += 2.5859e-3 * rpoly;
+  rpoly *= Rt; // ^3
+  temp -= 4.8260e-6 * rpoly;
+  rpoly *= Rt; // ^4
+  temp -= 2.8183e-8 * rpoly;
+  rpoly *= Rt; // ^5
+  temp += 1.5243e-10 * rpoly;
+
+  return temp;
+}
+
+uint8_t SensorMAX31865::readFault(max31865_fault_cycle_t fault_cycle) {
+  if (fault_cycle) {
+    uint8_t cfg_reg = readRegister8(MAX31865_CONFIG_REG);
+    cfg_reg &= 0x11; // mask out wire and filter bits
+    switch (fault_cycle) {
+    case MAX31865_FAULT_AUTO:
+      writeRegister8(MAX31865_CONFIG_REG, (cfg_reg | 0b10000100));
+      delay(1);
+      break;
+    case MAX31865_FAULT_MANUAL_RUN:
+      writeRegister8(MAX31865_CONFIG_REG, (cfg_reg | 0b10001000));
+      return 0;
+    case MAX31865_FAULT_MANUAL_FINISH:
+      writeRegister8(MAX31865_CONFIG_REG, (cfg_reg | 0b10001100));
+      return 0;
+    case MAX31865_FAULT_NONE:
+    default:
+      break;
+    }
+  }
+  return readRegister8(MAX31865_FAULTSTAT_REG);
+}
+
+
+
+
 void SensorMAX31865::setupReadRTD(void) {
 
-    mPT1000->clearFault();
-    mPT1000->enableBias(true);
+    clearFault();
+    enableBias(true);
     // delay(10);
 }
 
@@ -148,7 +265,7 @@ void SensorMAX31865::prepareReadRTD(void) {
 uint16_t SensorMAX31865::doReadRTD(void) {
 
     uint16_t rtd = readRegister16(MAX31865_RTDMSB_REG);
-    mPT1000->enableBias(false); // Disable bias current again to reduce selfheating.
+    enableBias(false); // Disable bias current again to reduce selfheating.
     // remove fault
     rtd >>= 1;
     return rtd;
@@ -177,13 +294,13 @@ uint16_t SensorMAX31865::readRegister16(uint8_t addr) {
 void SensorMAX31865::readRegisterN(uint8_t addr, uint8_t buffer[], uint8_t n)
 {
   addr &= 0x7F; // make sure top bit is not set
-  spi_dev.write_then_read(&addr, 1, buffer, n);
+  spi_dev->write_then_read(&addr, 1, buffer, n);
 }
 
 void SensorMAX31865::writeRegister8(uint8_t addr, uint8_t data) {
   addr |= 0x80; // make sure top bit is set
   uint8_t buffer[2] = {addr, data};
-  spi_dev.write(buffer, 2);
+  spi_dev->write(buffer, 2);
 }
 
 #endif
--- src/SensorMAX31865.h
+++ src/SensorMAX31865.h
@@ -1,15 +1,60 @@
 // Sensor MAX31865 - PT1000 interface
 
 #pragma once
-#if defined(SENSORMODULE) && defined(OPENKNX_SPI_MISO)
-  #include "Adafruit_MAX31865.h"
+#ifdef SENSORMODULE
+#include "Sensor.h"
+#ifdef OPENKNX_SPI_MISO
   #include "Sensor.h"
-  
+  #include "Adafruit_SPIDevice.h"
+
   // The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
   #define RREF      4300.0
   // The 'nominal' 0-degrees-C resistance of the sensor
   // 100.0 for PT100, 1000.0 for PT1000
   #define RNOMINAL  1000.0
+
+  #define MAX31865_CONFIG_REG 0x00
+  #define MAX31865_CONFIG_BIAS 0x80
+  #define MAX31865_CONFIG_MODEAUTO 0x40
+  #define MAX31865_CONFIG_MODEOFF 0x00
+  #define MAX31865_CONFIG_1SHOT 0x20
+  #define MAX31865_CONFIG_3WIRE 0x10
+  #define MAX31865_CONFIG_24WIRE 0x00
+  #define MAX31865_CONFIG_FAULTSTAT 0x02
+  #define MAX31865_CONFIG_FILT50HZ 0x01
+  #define MAX31865_CONFIG_FILT60HZ 0x00
+
+  #define MAX31865_RTDMSB_REG 0x01
+  #define MAX31865_RTDLSB_REG 0x02
+  #define MAX31865_HFAULTMSB_REG 0x03
+  #define MAX31865_HFAULTLSB_REG 0x04
+  #define MAX31865_LFAULTMSB_REG 0x05
+  #define MAX31865_LFAULTLSB_REG 0x06
+  #define MAX31865_FAULTSTAT_REG 0x07
+
+  #define MAX31865_FAULT_HIGHTHRESH 0x80
+  #define MAX31865_FAULT_LOWTHRESH 0x40
+  #define MAX31865_FAULT_REFINLOW 0x20
+  #define MAX31865_FAULT_REFINHIGH 0x10
+  #define MAX31865_FAULT_RTDINLOW 0x08
+  #define MAX31865_FAULT_OVUV 0x04
+
+  #define RTD_A 3.9083e-3
+  #define RTD_B -5.775e-7
+
+typedef enum max31865_numwires {
+  MAX31865_2WIRE = 0,
+  MAX31865_3WIRE = 1,
+  MAX31865_4WIRE = 0
+} max31865_numwires_t;
+
+typedef enum {
+  MAX31865_FAULT_NONE = 0,
+  MAX31865_FAULT_AUTO,
+  MAX31865_FAULT_MANUAL_RUN,
+  MAX31865_FAULT_MANUAL_FINISH
+} max31865_fault_cycle_t;
+
 
 class SensorMAX31865 : public Sensor
 {
@@ -22,7 +67,7 @@ class SensorMAX31865 : public Sensor
   };
 
   private:
-    Adafruit_MAX31865 *mPT1000;
+    Adafruit_SPIDevice *spi_dev;
     bool getSensorData();
 
   protected:
@@ -37,7 +82,14 @@ class SensorMAX31865 : public Sensor
     void setupReadRTD(void); 
     void prepareReadRTD(void); 
     uint16_t doReadRTD(void);
-    
+    void setWires(max31865_numwires_t wires);
+    void enableBias(bool b);
+    void autoConvert(bool b);
+    void setThresholds(uint16_t lower, uint16_t upper);
+    void clearFault(void);
+    float calculateTemperature(uint16_t RTDraw, float RTDnominal, float refResistor);
+    uint8_t readFault(max31865_fault_cycle_t fault_cycle = MAX31865_FAULT_AUTO);
+
     uint8_t readRegister8(uint8_t addr); 
     uint16_t readRegister16(uint8_t addr);
     void readRegisterN(uint8_t addr, uint8_t buffer[], uint8_t n);
@@ -52,4 +104,5 @@ class SensorMAX31865 : public Sensor
     uint8_t getI2cSpeed() override;
     std::string logPrefix() override;
   };
+#endif
 #endif
